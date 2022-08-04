6d8539acb5ce7c23b307e02ba3b8da29345e73be
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-08-04 14:45:12 +0300

    Update routeAPI.js

--- routeAPI.js
+++ routeAPI.js
@@ -1,2 +1,22 @@
 
-
+class routeAPI{
+  
+  constructor(port)
+  {
+    this.app = new express();
+    this.restServer = require('https').createServer(options, restApp);
+
+    restServer.listen(port, function() {
+    logger.writeLog('Listening on port ' + restPort);
+});
+  }
+}
+
+
+
+
+
+
+
+
+
