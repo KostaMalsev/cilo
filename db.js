7904f815ddb457035c80813ce3302f9ed2c93ca4
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-08-04 14:26:20 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
-
 const Pool = require('pg').Pool
 
-export class db{
-  
-  constructor(conn)
-  {
+export class db {
+
+  constructor(conn) {
     this.pool = new Pool(conn);
   }
 }
@@ -20,7 +18,7 @@ const pool = new Pool({
 })
 */
 
-const getUsers = (request, response) => {
+getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
     if (error) {
       throw error
@@ -28,73 +26,4 @@ const getUsers = (request, response) => {
     response.status(200).json(results.rows)
   })
 }
-
-
-
 }
-
-module.exports = {
-  getUsers
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
