50377cf92b34dee8bddc7015fbe815e95cae0e10
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-08-04 14:46:31 +0300

    Update routeAPI.js

--- routeAPI.js
+++ routeAPI.js
@@ -1,26 +1,20 @@
-
 const express = require('express')
 const bodyParser = require('body-parser')
 
 
-class routeAPI{
-  
-  constructor(port)
+class routeAPI {
+
+  constructor(port) 
   {
     this.app = new express();
-    this.restServer = require('https').createServer(options, restApp);
+    let options = {};
+    this.restServer = require('https').createServer(options, app);
 
     restServer.listen(port, function() {
-    logger.writeLog('Listening on port ' + restPort);
-});
+      logger.writeLog('Listening on port ' + port);
+    });
+    
   }
-}
-
-
-
-
-
-
-
+};
 
 
