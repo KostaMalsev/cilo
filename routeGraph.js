029a6ea511ed31ba7256694e8ee1a7ed82ff51ea
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-08-04 14:58:21 +0300

    Update routeGraph.js

--- routeGraph.js
+++ routeGraph.js
@@ -1,2 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+//additional imports
 
-
+
+class routeGraph {
+
+  constructor(port) {
+    super();
+    //Additonal setup...    
+  }
+
+  get(conn) {
+    //some other impl
+  }
+
+  getUsers(conn) {
+    //some other impl
+  }
+  
+};
