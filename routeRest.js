2a285b3ed937097a4673efe934f07c8d8a37873d
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2022-08-04 14:56:08 +0300

    Update routeRest.js

--- routeRest.js
+++ routeRest.js
@@ -1,2 +1,79 @@
 
-
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const {routeAPI} = import 'routeAPI.js'
+
+
+class routeRest {
+
+  constructor(port) {
+      super(port);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
